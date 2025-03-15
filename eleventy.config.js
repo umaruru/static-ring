9d52f433058fa7e1fad886e3ee31d27a175648d9
umaruru <deolindojvn@gmail.com>
2025-03-15 14:26:12 -0300

    clean up eleventy config file

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,8 +2,7 @@ import sites from "./sites.js";
 
 export const config = {
     dir: {
-        includes: 'includes',
-        // data: 'data',
+        includes: "includes",
     },
 
     markdownTemplateEngine: "njk",
@@ -12,22 +11,7 @@ export const config = {
 
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default async function(eleventyConfig) {
-    // eleventyConfig.addCollection("sites", collectionsApi => {
-    //     return sites.filter(site => site.enabled);
-    // });
-
-    // eleventyConfig.addCollection("urls", collectionsApi => {
-    //     console.log(collectionsApi.getAll())
-    //     return collectionsApi.getFilteredByTag("sites").map(site => site.url);
-    // })
-
     eleventyConfig.addGlobalData("sites", sites)
-
-    eleventyConfig.addGlobalData("enabledSites", sites.filter(site => site.enabled))
-
-    // eleventyConfig.addFilter("removeDisabled", sites => {
-    //     return sites.filter(site => site.enabled);
-    // });
 
     eleventyConfig.addFilter("getSitesUrls", sites => {
         return sites.map(site => site.url)
